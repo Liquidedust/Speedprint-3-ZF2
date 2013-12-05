facda28726f26d44d8b7cad29c094286e5551471
Stefan <Stefan@192.168.0.101>
2013-12-05 15:44:10 +0100

    Fixing with database category table design as well as menu category tree

---
+++ sql/select products in category node.sql
@@ -0,0 +1,21 @@
+SELECT
+	products.id_products AS product_id,
+	products.name_products AS product_name,
+	SUBSTR( products.description_products, 1, LOCATE( CHAR(10), products.description_products) ) AS product_preamble,
+# LEFT(subject , 10)
+	categories.name_categories AS product_category,
+	categories.id_categories AS product_category_id
+
+FROM
+	categories AS active_category,
+	products
+
+LEFT JOIN product_categories_rel AS relationships ON products.id_products = relationships.id_products
+LEFT JOIN categories ON relationships.id_categories = categories.id_categories
+
+WHERE
+	active_category.id_categories = :category_id
+	AND
+	categories.lft_categories BETWEEN active_category.lft_categories AND active_category.rgt_categories
+	AND
+	enabled_products = 'y'
